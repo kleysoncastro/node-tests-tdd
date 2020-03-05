4a1925f5e2f80d5cef9173e62ae88e3b5046b875
kleyson castro <kleysoncastro99@gmail.com>
2020-03-05 12:24:35 -0400

    chore: tests para email

--- __tests__/intregration/controller/Users.test.js
+++ __tests__/intregration/controller/Users.test.js
@@ -1,7 +1,12 @@
 import request from 'supertest';
 import app from '../../../src/app';
+import truncate from '../../util/truncate';
 
 describe('-----------  User  --------------', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
   it('Deve ser possivel fazer um cadastro', async () => {
     const response = await request(app)
       .post('/users')
@@ -11,5 +16,25 @@ describe('-----------  User  --------------', () => {
         password_hash: '12345',
       });
     expect(response.body).toHaveProperty('id');
+  });
+
+  it('Não pode ser possivel cadastral email duplicado', async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        name: 'kleyson',
+        email: 'kleyoncastro99@gmail.com',
+        password_hash: '12345',
+      });
+
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'kleyson',
+        email: 'kleyoncastro99@gmail.com',
+        password_hash: '12345',
+      });
+
+    expect(response.status).toBe(400);
   });
 });
--- src/app/controllers/UsersController.js
+++ src/app/controllers/UsersController.js
@@ -2,6 +2,14 @@ import User from '../models/Users';
 
 class UsersController {
   async store(req, res) {
+    const { email } = req.body;
+
+    const checkEmail = await User.findOne({ where: { email } });
+
+    if (checkEmail) {
+      return res.status(400).json({ erro: 'email já cadastrado' });
+    }
+
     const user = await User.create(req.body);
     return res.json(user);
   }
